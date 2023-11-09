bd58c3deef8eb281a6fbf4bcd66d780c5153cbb7
unknown <ffarzam_1992@yahoo.com>
2023-11-09 16:28:32 +0330

    refactor: update accounts microservice

--- Authentication/services/accounts_microservice.py
+++ Authentication/services/accounts_microservice.py
@@ -1,20 +1,25 @@
-import json
-
 import httpx
+
+from config.config import get_settings
+
+settings = get_settings()
 
 
 class Client:
-    register_url = "http://127.0.0.1:8001/accounts/v1/register"
-    login_url = "http://127.0.0.1:8001/accounts/v1/login"
+    register_url = settings.ACCOUNT_REGISTER_URL
 
-    async def register_call(self, payload: dict):
-        async with httpx.AsyncClient() as client:
+    login_url = settings.ACCOUNT_LOGIN_URL
+
+    async def register_call(self, payload: dict, request):
+        headers = {"unique_id": request.state.unique_id}
+        async with httpx.AsyncClient(headers=headers) as client:
             response = await client.post(self.register_url, json=payload)
         # response.raise_for_status()
         return response
 
-    async def login_call(self, payload: dict):
-        async with httpx.AsyncClient() as client:
+    async def login_call(self, payload: dict, request):
+        headers = {"unique_id": request.state.unique_id}
+        async with httpx.AsyncClient(headers=headers) as client:
             response = await client.post(self.login_url, json=payload)
         # response.raise_for_status()
         return response
