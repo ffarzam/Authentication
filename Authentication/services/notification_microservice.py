9a6fa76e943c5ac4032e5f06088ebe0336d16abc
unknown <ffarzam_1992@yahoo.com>
2023-11-09 16:29:20 +0330

    refactor: update notification microservice

--- Authentication/services/notification_microservice.py
+++ Authentication/services/notification_microservice.py
@@ -1,21 +1,17 @@
-import json
-
 import httpx
+
+from config.config import get_settings
+
+settings = get_settings()
 
 
 class Notification:
-    code_sender_url = "http://127.0.0.1:8002/notification/v1/code_sender"
-    login_url = "http://127.0.0.1:8002/accounts/v1/login"
+    code_sender_url = settings.NOTIFICATION_CODE_SENDER_URL
 
-    async def code_call(self, payload: dict):
-        async with httpx.AsyncClient() as client:
+    async def code_call(self, payload: dict, request):
+        headers = {"unique_id": request.state.unique_id}
+        async with httpx.AsyncClient(headers=headers) as client:
             response = await client.post(self.code_sender_url, json=payload)
-        # response.raise_for_status()
-        return response
-
-    async def login_call(self, payload: dict):
-        async with httpx.AsyncClient() as client:
-            response = await client.post(self.login_url, json=payload)
         # response.raise_for_status()
         return response
 
